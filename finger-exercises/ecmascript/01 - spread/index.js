08aa8ed5a05ab3032b834367bf9271bc5efd9b97
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2018-11-13 15:48:39 -0500

    Test 4 - spread

--- finger-exercises/ecmascript/01 - spread/index.js
+++ finger-exercises/ecmascript/01 - spread/index.js
@@ -1,6 +1,9 @@
 import isArray from './utils';
 
 export function min(...arg) {
+  if (arg.length === 0) {
+    return undefined;
+  }
   if (arg.length === 1) {
     if (isArray(arg[0])) {
       return Math.min(...arg[0]);
