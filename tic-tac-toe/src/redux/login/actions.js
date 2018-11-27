b3fcb4fa13361634ac5e4e4917c08192caec6256
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2018-11-27 11:04:35 -0500

    remove duplicate folders

--- tic-tac-toe/src/redux/login/actions.js
+++
@@ -1,8 +0,0 @@
-export const actions = {
-  CLICK_LOGIN: '@@LOGIN/CLICK_LOGIN'
-};
-
-export const clickLogin = (username, password) => ({
-  type: actions.CLICK_LOGIN,
-  payload: { username, password }
-});
