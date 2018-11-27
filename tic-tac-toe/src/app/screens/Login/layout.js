91e5ac36e530a499971d215d1c7ceccd62efefdb
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2018-11-27 11:02:13 -0500

    Renaming folders

---
+++ tic-tac-toe/src/app/screens/Login/layout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+import style from './styles.scss';
+
+function LoginForm(props) {
+  // eslint-disable-next-line
+  const { handleSubmit } = props;
+  return (
+    <form onSubmit={handleSubmit} className={style.login}>
+      <div>
+        <label htmlFor="username"> Username </label>
+        <Field name="username" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password"> Password </label>
+        <Field name="password" component="input" type="password" />
+      </div>
+      <button className={style.btn} type="submit">Sign in</button>
+    </form>
+  );
+}
+// eslint-disable-next-line
+LoginForm = reduxForm({
+  form: 'login'
+})(LoginForm);
+
+export default LoginForm;
