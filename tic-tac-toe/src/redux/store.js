659ba067e06db2e40eb7a0865a0f8f2f295bf338
Jeymarson Joel Garcia Sanmartin <jeymarson.garcia@wolox.co>
2018-12-07 12:14:26 -0500

    Redux devtools

--- tic-tac-toe/src/redux/store.js
+++ tic-tac-toe/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
@@ -16,7 +16,11 @@ const store = history => {
     router
   };
   const reducer = combineReducers(reducers);
-  return createStore(reducer, applyMiddleware(thunk, fetchMiddleware, routerMiddleware(history)));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, fetchMiddleware, routerMiddleware(history)))
+  );
 };
 
 export default store;
